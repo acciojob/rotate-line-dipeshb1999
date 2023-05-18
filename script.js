f02b83c7c17e93d195b01be1f63f02582085ab53
Badgujar Dipesh <117906055+dipeshb1999@users.noreply.github.com>
2023-05-18 11:57:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,2 @@
 //your JS code here. If required.
-     #line {
-            position: absolute;
-            width: 200px;
-            height: 2px;
-            background-color: rgb(0,0,0);
-            animation: rotate 2s linear infinite;
-        }
-
-        @keyframes rotate {
-            0% { transform: rotate(0deg); }
-            100% { transform: rotate(360deg); }
-        }
+        
